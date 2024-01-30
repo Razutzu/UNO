811733d7f49ad2a0347a7fee2b6b6420ff871f3c
Razutzu <81825697+Razutzu@users.noreply.github.com>
2024-01-30 17:44:27 +0200

    getReadyButton

--- src/Classes/game.js
+++ src/Classes/game.js
@@ -56,21 +56,6 @@ class Game {
 	//               GENERAL FUNCTIONS               //
 	///////////////////////////////////////////////////
 
-	lobby() {
-		this.embed = new EmbedBuilder().setColor(client.clr).setDescription("Game embed").addFields(this.usersToField());
-		this.components = [
-			new ActionRowBuilder().addComponents(
-				new ButtonBuilder().setCustomId("ready").setStyle(ButtonStyle.Success).setLabel("Ready"),
-				new ButtonBuilder().setCustomId("join").setStyle(ButtonStyle.Primary).setLabel("Join"),
-				new ButtonBuilder().setCustomId("leave").setStyle(ButtonStyle.Primary).setLabel("Leave")
-			),
-			new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("cpanel").setStyle(ButtonStyle.Danger).setLabel("Request Control Panel")),
-		];
-	}
-	game() {
-		this.embed = new EmbedBuilder().setColor(client.clr).setDescription("Game embed").addFields(this.playersToField());
-		this.components = [new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("gpanel").setStyle(ButtonStyle.Danger).setLabel("Request Game Panel"))];
-	}
 	async updateMessage() {
 		// updates the game message when in lobby
 		if (!this.message) this.message = await this.channel.send({ embeds: [this.embed], components: this.components }).catch((err) => client.err(err));
@@ -106,6 +91,21 @@ class Game {
 		}
 
 		return this.controlPanel.message;
+	}
+	lobby() {
+		this.embed = new EmbedBuilder().setColor(client.clr).setDescription("Game embed").addFields(this.usersToField());
+		this.components = [
+			new ActionRowBuilder().addComponents(
+				new ButtonBuilder().setCustomId("ready").setStyle(ButtonStyle.Success).setLabel("Ready"),
+				new ButtonBuilder().setCustomId("join").setStyle(ButtonStyle.Primary).setLabel("Join"),
+				new ButtonBuilder().setCustomId("leave").setStyle(ButtonStyle.Primary).setLabel("Leave")
+			),
+			new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("cpanel").setStyle(ButtonStyle.Danger).setLabel("Request Control Panel")),
+		];
+	}
+	game() {
+		this.embed = new EmbedBuilder().setColor(client.clr).setDescription("Game embed").addFields(this.playersToField());
+		this.components = [new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("gpanel").setStyle(ButtonStyle.Danger).setLabel("Request Game Panel"))];
 	}
 
 	///////////////////////////////////////////////////
@@ -208,91 +208,15 @@ class Game {
 		// return the join button
 		return this.components[0].components.find((c) => c.data.custom_id == "join");
 	}
+	getReadyButton() {
+		// returns the ready button
+		return this.components[0].components.find((c) => c.data.custom_id == "ready");
+	}
 
 	///////////////////////////////////////////////////
 	//                USERS FUNCTIONS                //
 	///////////////////////////////////////////////////
 
-	// async join(user) {
-	// 	// makes a user join the game
-	// 	if (this.users.length == 1) {
-	// 		this.getKickButton().setDisabled(false);
-	// 		this.getBanButton().setDisabled(false);
-	// 		this.getLockButton().setDisabled(false);
-	// 		this.getCHostButton().setDisabled(false);
-	// 	}
-
-	// 	if (this.controlPanelStatus == 1) this.updateUsersMenuOptions();
-
-	// 	this.users.push(new User(user, false, this));
-
-	// 	this.embed.setFields(this.usersToField());
-
-	// 	await this.updateControlPanel(null, false, false);
-	// 	return await this.updateMessage();
-	// }
-	// async leave(gameUser) {
-	// 	// makes a user leave the game
-	// 	if (this.users.length == 1) return this.end();
-
-	// 	this.removeUser(gameUser);
-
-	// 	if (gameUser.host) this.users[0].host = true;
-
-	// 	if (this.users.length == 1) {
-	// 		this.getKickButton().setDisabled(true);
-	// 		this.getBanButton().setDisabled(true);
-	// 		this.getLockButton().setDisabled(true);
-	// 		this.getCHostButton().setDisabled(true);
-	// 		this.resetMenu();
-	// 		if (this.locked) this.lock();
-	// 	}
-
-	// 	if (this.controlPanelStatus == 1) this.updateUsersMenuOptions();
-
-	// 	this.embed.setFields(this.usersToField());
-
-	// 	await this.updateControlPanel(null, false, gameUser.host);
-	// 	return await this.updateMessage();
-	// }
-	// async unban(gameUser) {
-	// 	// unbans a user
-	// 	this.banned.splice(this.banned.indexOf(gameUser), 1);
-
-	// 	if (this.banned.length == 0) {
-	// 		this.getUnbanButton().setDisabled(true);
-	// 		this.resetMenu();
-	// 	}
-
-	// 	await this.updateControlPanel(null, false, false);
-	// 	return await this.updateMessage();
-	// }
-	// async ban(gameUser) {
-	// 	// bans a user
-	// 	this.banned.push(gameUser);
-
-	// 	if (this.banned.length == 1) this.getUnbanButton().setDisabled(false);
-
-	// 	return await this.leave(gameUser);
-	// }
-	// async makeHost(gameUser) {
-	// 	// makes a user the host
-	// 	const hostUser = this.getHost();
-
-	// 	hostUser.host = false;
-	// 	gameUser.host = true;
-
-	// 	const gameUserIndex = this.users.indexOf(gameUser);
-
-	// 	this.users[0] = gameUser;
-	// 	this.users[gameUserIndex] = hostUser;
-
-	// 	this.resetMenu();
-	// 	this.embed.setFields(this.usersToField());
-
-	// 	await this.updateControlPanel(null, false, true);
-	// 	return await this.updateMessage();
-	// }
 	getUser(id) {
 		// returns a user
 		return this.users.find((u) => u.user.id === id);
@@ -350,7 +274,6 @@ class Game {
 		for (const player of this.players) {
 			if (player.isTurn()) value += `> **🎮 ${player.user.username} - ${player.cards.length}**\n`;
 			else value += `> 🎮 ${player.user.username} - ${player.cards.length}\n`;
-			console.log(1);
 		}
 
 		return {
